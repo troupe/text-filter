1acfe689cf8cc1070ec0fe6e6866a4d2123535a5
Andrew Newdigate <andrew@troupe.co>
2013-12-13 10:59:09 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 text-filter
 ===========
 
-A nano-scale module to match text in arrays
+A nano-scale module to match text in arrays.
+
+## Search String Arrays
+
+    var fixture = [
+     'Alley-Kat-Abra',
+     'Azrael',
+     'Bill the Cat',
+     'Blacksad',
+     'Bucky Katt',
+     'Catbert Dilbert'
+    ];
+
+    var result = fixture.filter(textFilter({ query: 'Kat' }));
+    //  result = [
+    //    'Alley-Kat-Abra',
+    //    'Bucky Katt'
+    //  ];
+
+
+
+## Search Object Arrays
+
+    var fixture = [
+     { name: 'Alley-Kat-Abra', description: 'Feline magician formerly with the Zoo Crew' },
+     { name: 'Azrael', description: 'Gargamel\'s red cat' },
+     { name: 'Bill the Cat', description: 'A largely comatose orange tabby' },
+     { name: 'Blacksad', description: 'Main character of graphic novel series.' },
+     { name: 'Bucky Katt', description: 'selfish, cynical, and lazy. His ears are nearly always drawn laid back flat on his head, a feline sign of defiance, aggressiveness and/or unfriendliness' },
+     { name: 'Catbert Dilbert', description: 'The evil human resources director' }
+    ];
+
+    var result = fixture.filter(textFilter({ query: 'Kat', fields: ['name', 'description'] }));
+    //  result = [
+    //    { name: 'Alley-Kat-Abra', ... },
+    //    { name: 'Bucky Katt', ...
+    //  ];
+
+
---
+++ test/text-filter-test.js
@@ -0,0 +1,30 @@
+var textFilter = require('../lib/text-filter');
+var assert = require('assert');
+
+var fixture = [
+ { name: 'Alley-Kat-Abra', description: 'Feline magician formerly with the Zoo Crew' },
+ { name: 'Azrael', description: 'Gargamel\'s red cat' },
+ { name: 'Bill the Cat', description: 'A largely comatose orange tabby' },
+ { name: 'Blacksad', description: 'Main character of graphic novel series.' },
+ { name: 'Bucky Katt', description: 'selfish, cynical, and lazy. His ears are nearly always drawn laid back flat on his head, a feline sign of defiance, aggressiveness and/or unfriendliness' },
+ { name: 'Catbert Dilbert', description: 'The evil human resources director' }
+];
+
+var simpleFixture = fixture.map(function(f) { return f.description; });
+
+describe('text-filter', function() {
+
+  it('should find the right phrases in a text array', function() {
+    var result = simpleFixture.filter(textFilter({ query: 'resour' }));
+    assert.equal(result.length, 1);
+    assert.equal(result[0], 'The evil human resources director');
+  });
+
+  it('should find the right phrases in a text array', function() {
+    var result = fixture.filter(textFilter({ query: 'Kat', fields: ['name', 'description'] }));
+    assert.equal(result.length, 2);
+    assert.equal(result[0].name, 'Alley-Kat-Abra');
+    assert.equal(result[1].name, 'Bucky Katt');
+  });
+
+});
